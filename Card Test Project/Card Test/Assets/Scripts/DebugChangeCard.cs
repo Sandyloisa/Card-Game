39eec83c62db8daaaedd02aec29db44839b0ab37
Sandyloisa <sandeep_saha@Sandeep-Sahas-Mac.local>
2017-10-02 09:58:41 -0700

    Card Game Assignment Project

---
+++ Card Test Project/Card Test/Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour {
+
+	CardModel cardModel;
+	int cardIndex=0;
+	public GameObject card;
+
+	void Awake()
+	{
+		cardModel = card.GetComponent <CardModel> ();
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (10, 10, 100, 28), "Hit me")) {
+			
+			if (cardIndex >=cardModel.faces.Length) {
+				cardIndex = 0;
+				cardModel.ToggleFace (false);
+			} else {
+				cardModel.cardIndex = cardIndex;
+				cardModel.ToggleFace (true);
+				cardIndex++;
+			}
+
+
+		}
+	}
+}
---
+++ Card Test Project/Card Test/Assets/Scripts/DeckView.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Deck))]
+public class DeckView : MonoBehaviour {
+
+	Deck deck;
+
+	public Vector3 start;
+	public GameObject cardPrefab;
+	public float cardOffset;
+
+	void Start()
+	{
+		deck = GetComponent<Deck> ();
+		ShowCards ();
+	}
+	void ShowCards()
+	{
+		int cardCount = 0;
+
+		foreach (int i in deck.GetCards()) {
+
+			float co = cardOffset * cardCount;
+
+			GameObject cardCopy = (GameObject)Instantiate (cardPrefab);
+			Vector3 temp = start + new Vector3 (0f, co);
+			cardCopy.transform.position = temp;
+
+			CardModel cardModel = cardCopy.GetComponent<CardModel> ();
+			cardModel.cardIndex = i;
+			cardModel.ToggleFace (true);
+
+			SpriteRenderer spriteRenderer = cardCopy.GetComponent<SpriteRenderer> ();
+			spriteRenderer.sortingOrder = 51- cardCount;
+
+
+			cardCount++;
+		}
+	}
+}
